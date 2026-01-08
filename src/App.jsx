695498e9c5a466b37602f6dd787a559050230cfb
Mohamed Basha <36714620+mohamedbasha275@users.noreply.github.com>
2026-01-08 15:40:16 +0200

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfolio by Mohammed Basha</h1>
+      <p>Loading...</p>
+    </div>
+  )
+}
+
+export default App
