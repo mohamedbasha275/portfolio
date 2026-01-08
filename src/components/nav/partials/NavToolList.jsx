b337bb4eb088dd1b60e1e0d69ab2e3e80523117b
Mohamed Basha <36714620+mohamedbasha275@users.noreply.github.com>
2026-01-08 13:00:48 +0200

    complete my profile

--- src/components/nav/partials/NavToolList.jsx
+++ src/components/nav/partials/NavToolList.jsx
@@ -1,17 +1,14 @@
 import "./NavToolList.scss"
 import React, {useEffect, useState} from 'react'
-import {useLanguage} from "/src/providers/LanguageProvider.jsx"
 import {useTheme} from "/src/providers/ThemeProvider.jsx"
 import {useFeedbacks} from "/src/providers/FeedbacksProvider.jsx"
 import {useData} from "/src/providers/DataProvider.jsx"
-import NavToolLanguagePicker from "/src/components/nav/tools/NavToolLanguagePicker.jsx"
 import NavToolThemePicker from "/src/components/nav/tools/NavToolThemePicker.jsx"
 import NavToolCursorToggle from "/src/components/nav/tools/NavToolCursorToggle.jsx"
 import NavToolSettings from "/src/components/nav/tools/NavToolSettings.jsx"
 import NavToolResumeDownloader from "/src/components/nav/tools/NavToolResumeDownloader.jsx"
 
 function NavToolList({ expanded }) {
-    const language = useLanguage()
     const theme = useTheme()
     const feedbacks = useFeedbacks()
     const data = useData()
@@ -24,7 +21,6 @@ function NavToolList({ expanded }) {
         `nav-tools-shrink`
 
     const widgets = [
-        ...(language.supportsMultipleLanguages ? ["language"] : []),
         ...(theme.supportsMultipleThemes ? [NavToolSettings.Options.THEME] : []),
         ...(feedbacks.animatedCursorEnabled ? [NavToolSettings.Options.CURSOR] : []),
         ...(profile.resumePdfUrl ? [NavToolSettings.Options.DOWNLOAD_RESUME] : []),
@@ -43,7 +39,6 @@ function NavToolList({ expanded }) {
             {visibleWidgets.map((item, key) => (
                 <div className={`nav-tools-item`}
                      key={key}>
-                    {item === "language" && (<NavToolLanguagePicker/>)}
                     {item === NavToolSettings.Options.THEME && (<NavToolThemePicker/>)}
                     {item === NavToolSettings.Options.CURSOR && (<NavToolCursorToggle/>)}
                     {item === NavToolSettings.Options.DOWNLOAD_RESUME && (<NavToolResumeDownloader/>)}
